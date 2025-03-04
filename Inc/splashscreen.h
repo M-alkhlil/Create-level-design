295c10915a4d818364d1d25bc3378eb294bf58d9
M-alkhlil <m.alkhlil@hotmail.com>
2025-03-04 22:40:39 +0800

    Updated splashscreen.h

--- Inc/splashscreen.h
+++ Inc/splashscreen.h
@@ -1,21 +1,21 @@
- 
+#ifndef SPLASHSCREEN_H
+#define SPLASHSCREEN_H
 
 #include "cprocessing.h"
 
-enum SplashScreenState
-{
+/// Enum for the splash screen states.
+typedef enum {
     DIGIPENLOGO,     // State for displaying the DigiPen logo
-    SPLASH_SCREEN,   // State for displaying the studio logo
-};
+    SPLASH_SCREEN    // State for displaying the studio logo
+} SplashScreenState;
 
 /// Initializes the splash screen state.
-
 void InitializeSplashScreen(void);
-// Updates the splash screen state. 
-//Handles transitions between logos and to the main menu.
-void UpdateSplashScreen(void);
-/// Cleans up resources used by the splash screen (e.g., freeing images).
 
+/// Updates the splash screen state, handling transitions between logos and the main menu.
+void UpdateSplashScreen(void);
+
+/// Cleans up resources used by the splash screen (e.g., freeing images).
 void ExitSplashScreen(void);
 
- 
+#endif // SPLASHSCREEN_H
